fb06667e2b32c52561cf48b1eace1c73788d0faf
JUNU <bokjunwoo@gmail.com>
2023-09-10 23:40:51 +0900

    feat : maple api 요청 추가

--- back/app.ts
+++ back/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import axios from 'axios';
 
 import dotenv from 'dotenv';
 dotenv.config();
@@ -23,6 +24,25 @@ app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req: express.Request, res: express.Response) => {
   res.status(200).send('maple_cube API');
+});
+
+app.get('/api/data', async (req, res) => {
+  try {
+    const response = await axios.get(
+      'https://public.api.nexon.com/openapi/maplestory/v1/cube-use-results',
+      {
+        params: req.query,
+        withCredentials: true,
+        headers: {
+          Authorization: req.query.key as string,
+        },
+      }
+    );
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'An error occurred' });
+  }
 });
 
 /* start */
