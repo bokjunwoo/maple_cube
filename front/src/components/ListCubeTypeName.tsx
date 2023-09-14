68354c47e2af7d465eb0063a9d18150484270751
JUNU <bokjunwoo@gmail.com>
2023-09-14 22:17:34 +0900

    feat : 사용한 큐브 타입의 사용 개수 구하는 기능 추가

--- front/src/components/ListCubeTypeName.tsx
+++ front/src/components/ListCubeTypeName.tsx
@@ -5,6 +5,18 @@ type ListCubeTypeNameType = {
 };
 
 export const ListCubeType = ({ data }: ListCubeTypeNameType) => {
+  const cubeTypeCounts: { [cubeTypeName: string]: number } = {};
+
+  // 데이터를 반복하여 개수를 계산
+  data.forEach((item) => {
+    const cubeTypeName = item.cube_type;
+    if (cubeTypeCounts[cubeTypeName]) {
+      cubeTypeCounts[cubeTypeName]++;
+    } else {
+      cubeTypeCounts[cubeTypeName] = 1;
+    }
+  });
+
   const cubeTypeNames = [...new Set(data.map((item) => item.cube_type))];
 
   return (
@@ -12,7 +24,9 @@ export const ListCubeType = ({ data }: ListCubeTypeNameType) => {
       <h3>사용한 큐브</h3>
       <ul>
         {cubeTypeNames.map((cubeTypeName) => (
-          <li key={cubeTypeName}>{cubeTypeName}</li>
+          <li key={cubeTypeName}>
+            {cubeTypeName} ({cubeTypeCounts[cubeTypeName] || 0}개)
+          </li>
         ))}
       </ul>
     </>
