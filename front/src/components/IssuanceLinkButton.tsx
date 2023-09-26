647e05a820721b6a8075904675f955da6c1981ac
JUNU <bokjunwoo@gmail.com>
2023-09-27 01:32:31 +0900

    fix design : mt 삭제

--- front/src/components/IssuanceLinkButton.tsx
+++ front/src/components/IssuanceLinkButton.tsx
@@ -3,12 +3,7 @@ import { CustomButtonUI } from './ui/CustomButtonUI';
 
 export const IssuanceLinkButton = () => {
   return (
-    <Link
-      href="#"
-      target="_blank"
-      underline="none"
-      sx={{ display: 'flex', mt: 3 }}
-    >
+    <Link href="#" target="_blank" underline="none" sx={{ display: 'flex' }}>
       <CustomButtonUI
         width="100%"
         height="56px"
