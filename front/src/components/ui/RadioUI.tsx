bfd02bd6cd1e11e6101e3704ae902b37d1ed54e5
JUNU <bokjunwoo@gmail.com>
2023-09-28 18:57:57 +0900

    refactor : info 데이터를 통한 컴포넌트 구조 변경

--- front/src/components/ui/RadioUI.tsx
+++ front/src/components/ui/RadioUI.tsx
@@ -4,6 +4,9 @@ import {
   RadioGroup,
   FormControlLabel,
   Radio,
+  Avatar,
+  Box,
+  Typography,
 } from '@mui/material';
 
 type RadioUIType = {
@@ -11,6 +14,13 @@ type RadioUIType = {
   label: string;
   handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   countKeyValue: { [key: string]: number };
+  info: {
+    [key: string]: {
+      color: string;
+      name?: string;
+      image?: string;
+    };
+  };
 };
 
 export const RadioUI = ({
@@ -18,27 +28,79 @@ export const RadioUI = ({
   label,
   handleChange,
   countKeyValue,
+  info,
 }: RadioUIType) => {
   return (
     <FormControl fullWidth>
-      <FormLabel id="controlled-radio-buttons-group">{label}</FormLabel>
+      <FormLabel
+        id="controlled-radio-buttons-group"
+        sx={{ fontSize: '20px', color: 'black' }}
+      >
+        {label}
+      </FormLabel>
+
       <RadioGroup
         row
         aria-labelledby="controlled-radio-buttons-group"
         name="controlled-radio-buttons-group"
         onChange={handleChange}
       >
-        {data.map((item) => {
-          return (
-            <div key={item}>
-              <FormControlLabel
-                value={item}
-                control={<Radio />}
-                label={`${item} (${countKeyValue[item]}개)`}
-              />
-            </div>
-          );
-        })}
+        {data.map((item) => (
+          <div key={item}>
+            <FormControlLabel
+              value={item}
+              control={
+                <Radio
+                  sx={{
+                    color: info[item].color || 'defaultColor',
+                    '&.Mui-checked': {
+                      color: info[item].color || 'defaultColor',
+                    },
+                  }}
+                />
+              }
+              label={
+                <div>
+                  {info[item].image && (
+                    <Box sx={{ display: 'flex' }}>
+                      <Avatar
+                        variant="rounded"
+                        sx={{ width: 24, height: 24, mr: 0.5 }}
+                        src={info[item].image}
+                        alt={item}
+                      />
+                      <Typography
+                        variant="body1"
+                        sx={{ color: info[item].color }}
+                      >{`${item} (${countKeyValue[item]}개)`}</Typography>
+                    </Box>
+                  )}
+                  {info[item].name && (
+                    <Box sx={{ display: 'flex' }}>
+                      <Avatar
+                        variant="rounded"
+                        sx={{
+                          width: 18,
+                          height: 18,
+                          mr: 0.5,
+                          mt: 0.4,
+                          bgcolor: info[item].color,
+                          fontSize: '16px',
+                        }}
+                      >
+                        {info[item].name}
+                      </Avatar>
+                      <Typography
+                        variant="body1"
+                        sx={{ color: info[item].color }}
+                      >{`${item} (${countKeyValue[item]}개)`}</Typography>
+                    </Box>
+                  )}
+                </div>
+              }
+            />
+          </div>
+        ))}
       </RadioGroup>
     </FormControl>
   );
