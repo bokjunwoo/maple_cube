0f7135915b67e1e3bea42d025f5a789b29800867
JUNU <bokjunwoo@gmail.com>
2023-09-29 23:46:00 +0900

    refactor : AccordionUI 에서 currentPage state 관리 변경

--- front/src/components/SelectCubeGrade.tsx
+++ front/src/components/SelectCubeGrade.tsx
@@ -1,5 +1,10 @@
+import { useRecoilState } from 'recoil';
 import { CubeHistory } from '../api/api';
 import { AccordionUI } from './ui/AccordionUI';
+import { currentPageState } from '../atom/cubeDataState';
+import { Box } from '@mui/material';
+import { ITEMS_COUNT_PER_PAGE } from '../constants/units';
+import { PaginationUI } from './ui/PaginationUI';
 
 type SelectCubeGradeType = {
   filterdata: CubeHistory[];
@@ -14,6 +19,8 @@ export const SelectCubeGrade = ({
   potentialName,
   selectedCubeName,
 }: SelectCubeGradeType) => {
+  const [currentPage, setCurrentPage] = useRecoilState(currentPageState);
+
   const gradeName =
     potentialName === '에디셔널'
       ? 'additional_potential_option_grade'
@@ -23,7 +30,34 @@ export const SelectCubeGrade = ({
     (item) => item[gradeName] === selectedCubeGrade
   );
 
+  const handlePageChange = (
+    event: React.ChangeEvent<unknown>,
+    newPage: number
+  ) => {
+    setCurrentPage(newPage);
+  };
+
+  const startIndex = (currentPage - 1) * ITEMS_COUNT_PER_PAGE;
+  const endIndex = startIndex + 20;
+  const currentData = selectedGrades.slice(startIndex, endIndex);
+
+  const filterDataLength = filterdata.length;
+
   return (
-    <AccordionUI data={selectedGrades} selectedCubeName={selectedCubeName} />
+    <>
+      <AccordionUI
+        data={currentData}
+        selectedCubeName={selectedCubeName}
+        startIndex={startIndex}
+        filterDataLength={filterDataLength}
+      />
+      <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2, mb: 1 }}>
+        <PaginationUI
+          count={Math.ceil(filterdata.length / ITEMS_COUNT_PER_PAGE)}
+          onChange={handlePageChange}
+          page={currentPage}
+        />
+      </Box>
+    </>
   );
 };
--- front/src/components/ui/AccordionUI.tsx
+++ front/src/components/ui/AccordionUI.tsx
@@ -17,35 +17,26 @@ import { WhiteAdditionalCubeOptionUI } from './WhiteAdditionalCubeOptionUI';
 import { AdditionalCubeOptionUI } from './AdditionalCubeOptionUI';
 import { BlackCubeOptionUI } from './BlackCubeOptionUI';
 import { CommonCubeOptionUI } from './CommonCubeOptionUI';
-import { useState } from 'react';
-import { PaginationUI } from './PaginationUI';
-import { ITEMS_COUNT_PER_PAGE } from '../../constants/units';
 
 type AccordionUIType = {
   data: CubeHistory[];
-  selectedCubeType: string;
+  selectedCubeName: string;
+  startIndex: number;
+  filterDataLength: number;
 };
 
-export const AccordionUI = ({ data, selectedCubeType }: AccordionUIType) => {
-  const [currentPage, setCurrentPage] = useState(1);
-
-  const handlePageChange = (
-    event: React.ChangeEvent<unknown>,
-    newPage: number
-  ) => {
-    setCurrentPage(newPage);
-  };
-
-  const startIndex = (currentPage - 1) * 20;
-  const endIndex = startIndex + 20;
-  const currentData = data.slice(startIndex, endIndex);
-
-  const isInCommonCube = COMMON_CUBE.includes(selectedCubeType);
-  const isInAdditionalCube = COMMON_ADDITIONAL_CUBE.includes(selectedCubeType);
+export const AccordionUI = ({
+  data,
+  selectedCubeName,
+  startIndex,
+  filterDataLength,
+}: AccordionUIType) => {
+  const isInCommonCube = COMMON_CUBE.includes(selectedCubeName);
+  const isInAdditionalCube = COMMON_ADDITIONAL_CUBE.includes(selectedCubeName);
 
   return (
     <Box>
-      {currentData.map((item, i) => {
+      {data.map((item, i) => {
         return (
           <Accordion key={item.id}>
             <AccordionSummary
@@ -54,20 +45,20 @@ export const AccordionUI = ({ data, selectedCubeType }: AccordionUIType) => {
               id={`panel-content${i + 1}`}
             >
               <Typography variant="subtitle1">
-                큐브 옵션 {data.length - (startIndex + i)}
+                큐브 옵션 {filterDataLength - (startIndex + i)}
               </Typography>
             </AccordionSummary>
             <AccordionDetails>
               <div>
                 {isInCommonCube && <CommonCubeOptionUI data={item} />}
 
-                {selectedCubeType === CUBE_NAME.BLACK_CUBE && (
+                {selectedCubeName === CUBE_NAME.BLACK_CUBE && (
                   <BlackCubeOptionUI data={item} />
                 )}
 
                 {isInAdditionalCube && <AdditionalCubeOptionUI data={item} />}
 
-                {selectedCubeType === CUBE_NAME.WHITE_ADDITIONAL_CUBE && (
+                {selectedCubeName === CUBE_NAME.WHITE_ADDITIONAL_CUBE && (
                   <WhiteAdditionalCubeOptionUI data={item} />
                 )}
               </div>
@@ -75,14 +66,6 @@ export const AccordionUI = ({ data, selectedCubeType }: AccordionUIType) => {
           </Accordion>
         );
       })}
-
-      <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2, mb: 1 }}>
-        <PaginationUI
-          count={Math.ceil(data.length / ITEMS_COUNT_PER_PAGE)}
-          onChange={handlePageChange}
-          page={currentPage}
-        />
-      </Box>
     </Box>
   );
 };
