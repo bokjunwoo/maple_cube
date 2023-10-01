0228cd3d780386b67f4daa87bf1596f0b29049d0
JUNU <bokjunwoo@gmail.com>
2023-10-01 16:51:53 +0900

    feat : NoResultsPageUI(검색결과없음)컴포넌트 구현

---
+++ front/src/components/ui/NoResultsPageUI.tsx
@@ -0,0 +1,48 @@
+import { AlertTitle, Box, Link } from '@mui/material';
+import { CustomButtonUI } from './CustomButtonUI';
+import { ErrorOutline } from '@mui/icons-material';
+
+type NoResultsPageUIType = {
+  startDate: string;
+  endDate: string;
+};
+
+export const NoResultsPageUI = ({
+  startDate,
+  endDate,
+}: NoResultsPageUIType) => {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'column',
+      }}
+    >
+      <img
+        src="/images/no_data.png"
+        alt="요청된 데이터 없음"
+        style={{ width: '300px' }}
+      />
+
+      <Box sx={{ display: 'flex', mt: 3 }}>
+        <ErrorOutline color="error" sx={{ mr: 1 }} />
+        <AlertTitle>
+          요청하신 {startDate} ~ {endDate}의 큐브데이터는 없습니다.
+        </AlertTitle>
+      </Box>
+
+      <Link sx={{ mt: 3, width: '100%' }} underline="none" href="/">
+        <CustomButtonUI
+          width="100%"
+          height="56px"
+          text="다시 검색하러가기"
+          bgColor="#D0E8F2"
+          color="black"
+          variant="button"
+          hoverBorder="2px solid #057cad"
+        />
+      </Link>
+    </Box>
+  );
+};
