e3d514dc3e4b2219018b0a9530e087e0dfc54f69
JUNU <bokjunwoo@gmail.com>
2023-10-02 23:45:43 +0900

    design : HeaderUI 디자인 변경

--- front/src/components/ui/HeaderUI.tsx
+++ front/src/components/ui/HeaderUI.tsx
@@ -2,20 +2,20 @@ import { AppBar, Container, Toolbar, Typography } from '@mui/material';
 
 export const HeaderUI = () => {
   return (
-    <AppBar position="static">
+    <AppBar position="static" sx={{ backgroundColor: 'white', boxShadow: 0 }}>
       <Container maxWidth="sm">
-        <Toolbar sx={{ height: '80px' }} disableGutters>
+        <Toolbar sx={{ height: '90px' }} disableGutters>
           <Typography
             variant="h3"
             component="a"
             href="/"
             sx={{
-              fontWeight: 700,
-              color: 'inherit',
+              fontWeight: 900,
+              color: '#3EC1D3',
               textDecoration: 'none',
             }}
           >
-            큐브 사용
+            큐브데이터
           </Typography>
         </Toolbar>
       </Container>
