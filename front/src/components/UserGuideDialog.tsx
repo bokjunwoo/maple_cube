3583c931de7467f29dc2d0efa41df8a2c530b612
JUNU <bokjunwoo@gmail.com>
2023-10-13 21:54:59 +0900

    refactor : GuideTextUI로 구조 변경

--- front/src/components/UserGuideDialog.tsx
+++ front/src/components/UserGuideDialog.tsx
@@ -10,11 +10,10 @@ import {
   useMediaQuery,
   Box,
   Container,
-  CircularProgress,
-  Typography,
 } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
 import dayjs from 'dayjs';
+import { GuideTextUI } from './ui/GuideTextUI';
 
 type UserGuideDialogType = {
   open: boolean;
@@ -91,389 +90,125 @@ export const UserGuideDialog = ({ open, handleClose }: UserGuideDialogType) => {
       <DialogContent dividers sx={{ p: 0 }}>
         <Container>
           <Box sx={{ mt: 2, mb: 2 }}>
-            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-              <Box sx={{ display: 'inline-flex', mb: 1 }}>
-                <Typography
-                  variant="h6"
-                  sx={{ display: 'inline-block', mr: 1 }}
-                >
-                  ◈
-                </Typography>
-                <Typography variant="h5" sx={{ fontWeight: 900 }}>
-                  API키 발급
-                </Typography>
-              </Box>
-            </Box>
-            <Box sx={{ display: 'inline-flex' }}>
-              <Typography
-                variant="h6"
-                sx={{ display: 'inline-block', mr: 1, visibility: 'hidden' }}
-              >
-                ◈
-              </Typography>
-              <Typography variant="body2" sx={{ mb: 1 }}>
-                발급에 관한 자세한 사항은 API 키 발급 방법을 클릭해 확인해
-                주세요.
-              </Typography>
-            </Box>
+            <GuideTextUI
+              title="API키 발급"
+              isImage={false}
+              description={[
+                {
+                  text: '발급에 관한 자세한 사항은 API 키 발급 방법을 클릭해 확인해주세요.',
+                  coloredTexts: [''],
+                },
+              ]}
+            />
           </Box>
 
           <Box sx={{ mb: 2 }}>
-            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-              <Box sx={{ display: 'inline-flex', mb: 1 }}>
-                <Typography
-                  variant="h6"
-                  sx={{ display: 'inline-block', mr: 1 }}
-                >
-                  ◈
-                </Typography>
-                <Typography variant="h5" sx={{ fontWeight: 900 }}>
-                  날짜 선택
-                </Typography>
-              </Box>
-            </Box>
-            <Box sx={{ display: 'inline-flex' }}>
-              <Typography
-                variant="h6"
-                sx={{ display: 'inline-block', mr: 1, visibility: 'hidden' }}
-              >
-                ◈
-              </Typography>
-              <Typography variant="body2" sx={{ mb: 1 }}>
-                검색을 원하는 요일을 선택합니다. Nexon Developers 정책에 따라{' '}
-                <span style={{ color: '#ff4c4c' }}>2022년 11월 25일 이후</span>
-                부터 데이터 검색이 가능하며 이전 데이터의 경우 요청이
-                불가능합니다. 또한, 오늘
-                <span style={{ color: '#ff4c4c' }}>({today})</span>의 데이터는
-                다음날
-                <span style={{ color: '#ff4c4c' }}>
-                  ({tomorrow} 오전 5시 이후)
-                </span>
-                부터 요청이 가능하며, 일 최대 조회할 수 있는 큐브 데이터는
-                1,000개로 일 최대 데이터 개수를 넘는 큐브를 사용할 때 해당
-                정보는 제공되지 않습니다. 데이터를 요청하는 날짜가 긴 경우
-                시간이 오래 소요될 수 있습니다.
-              </Typography>
-            </Box>
-          </Box>
-
-          <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-            <Box sx={{ display: 'inline-flex', mb: 1 }}>
-              <Typography variant="h6" sx={{ display: 'inline-block', mr: 1 }}>
-                ◈
-              </Typography>
-              <Typography variant="h5" sx={{ fontWeight: 900 }}>
-                사용법
-              </Typography>
-            </Box>
-            <Box sx={{ display: 'inline-flex' }}>
-              <Typography
-                variant="h6"
-                sx={{ display: 'inline-block', mr: 1, visibility: 'hidden' }}
-              >
-                ◈
-              </Typography>
-              <Box>
-                <Typography variant="body2" sx={{ mb: 1 }}>
-                  데이터를 확인하고 싶은{' '}
-                  <span style={{ color: '#ff4c4c' }}>캐릭터</span>를 선택해
-                  주세요.
-                </Typography>
-                {!imageLoaded1 && (
-                  <Box
-                    sx={{
-                      width: '332.7px',
-                      height: '130px',
-                      display: 'flex',
-                      justifyContent: 'center',
-                      alignItems: 'center',
-                    }}
-                  >
-                    <CircularProgress />
-                  </Box>
-                )}
-                <img
-                  src="/images/select_character.gif"
-                  alt=""
-                  style={{
-                    maxWidth: '100%',
-                    height: '130px',
-                  }}
-                  onLoad={handleImageLoad1}
-                />
-              </Box>
-            </Box>
-
-            <Box sx={{ display: 'inline-flex' }}>
-              <Typography
-                variant="h6"
-                sx={{ display: 'inline-block', mr: 1, visibility: 'hidden' }}
-              >
-                ◈
-              </Typography>
-              <Box>
-                <Typography variant="body2">
-                  캐릭터 선택 후{' '}
-                  <span style={{ color: '#ff4c4c' }}>아이템</span>을 선택해
-                  주세요.
-                </Typography>
-                {!imageLoaded2 && (
-                  <Box
-                    sx={{
-                      width: '332.7px',
-                      height: '130px',
-                      display: 'flex',
-                      justifyContent: 'center',
-                      alignItems: 'center',
-                    }}
-                  >
-                    <CircularProgress />
-                  </Box>
-                )}
-                <img
-                  src="/images/select_item.gif"
-                  alt=""
-                  style={{
-                    maxWidth: '100%',
-                    height: '130px',
-                  }}
-                  onLoad={handleImageLoad2}
-                />
-              </Box>
-            </Box>
-
-            <Box sx={{ display: 'inline-flex' }}>
-              <Typography
-                variant="h6"
-                sx={{ display: 'inline-block', mr: 1, visibility: 'hidden' }}
-              >
-                ◈
-              </Typography>
-              <Box>
-                <Typography variant="body2" sx={{ mb: 1 }}>
-                  데이터를 확인하고 싶은{' '}
-                  <span style={{ color: '#ff4c4c' }}>큐브</span>를 선택해
-                  주세요.
-                </Typography>
-                {!imageLoaded3 && (
-                  <Box
-                    sx={{
-                      width: '332.7px',
-                      height: '75px',
-                      display: 'flex',
-                      justifyContent: 'center',
-                      alignItems: 'center',
-                    }}
-                  >
-                    <CircularProgress />
-                  </Box>
-                )}
-                <img
-                  src="/images/select_cubename.gif"
-                  alt=""
-                  style={{
-                    maxWidth: '100%',
-                    height: '75px',
-                  }}
-                  onLoad={handleImageLoad3}
-                />
-              </Box>
-            </Box>
-
-            <Box sx={{ display: 'inline-flex' }}>
-              <Typography
-                variant="h6"
-                sx={{ display: 'inline-block', mr: 1, visibility: 'hidden' }}
-              >
-                ◈
-              </Typography>
-              <Box>
-                <Typography variant="body2" sx={{ mb: 1 }}>
-                  데이터를 확인하고 싶은{' '}
-                  <span style={{ color: '#ff4c4c' }}>큐브 옵션</span>을 선택해
-                  주세요.
-                </Typography>
-                {!imageLoaded4 && (
-                  <Box
-                    sx={{
-                      width: '332.7px',
-                      height: '75px',
-                      display: 'flex',
-                      justifyContent: 'center',
-                      alignItems: 'center',
-                    }}
-                  >
-                    <CircularProgress />
-                  </Box>
-                )}
-                <img
-                  src="/images/selec_cubetype.gif"
-                  alt=""
-                  style={{
-                    maxWidth: '100%',
-                    height: '75px',
-                  }}
-                  onLoad={handleImageLoad4}
-                />
-              </Box>
-            </Box>
+            <GuideTextUI
+              title="날짜 선택"
+              isImage={false}
+              description={[
+                {
+                  text: `검색을 원하는 요일을 선택합니다. Nexon Developers 정책에 따라 2022년 11월 25일 이후부터 데이터 검색이 가능하며 이전 데이터의 경우 요청이 불가능합니다. 또한, 오늘(${today})의 데이터는 다음날(${tomorrow} 오전 5시 이후)부터 요청이 가능하며, 일 최대 조회할 수 있는 큐브 데이터는 1,000개로 일 최대 데이터 개수를 넘는 큐브를 사용할 때 해당 정보는 제공되지 않습니다. 데이터를 요청하는 날짜가 긴 경우 시간이 오래 소요될 수 있습니다.`,
+                  coloredTexts: [
+                    '2022년 11월 25일 이후',
+                    `(${today})`,
+                    `(${tomorrow} 오전 5시 이후)`,
+                  ],
+                },
+              ]}
+            />
           </Box>
 
           <Box sx={{ mb: 2 }}>
-            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-              <Box sx={{ display: 'inline-flex', mb: 1 }}>
-                <Typography
-                  variant="h6"
-                  sx={{ display: 'inline-block', mr: 1 }}
-                >
-                  ◈
-                </Typography>
-                <Typography variant="h5" sx={{ fontWeight: 900 }}>
-                  큐브 옵션 확인
-                </Typography>
-              </Box>
-            </Box>
-            <Box sx={{ display: 'inline-flex' }}>
-              <Typography
-                variant="h6"
-                sx={{ display: 'inline-block', mr: 1, visibility: 'hidden' }}
-              >
-                ◈
-              </Typography>
-              <Box>
-                <Typography variant="body2" sx={{ mb: 1 }}>
-                  큐브 등급 업을 확인하고 싶은 경우 해당 등급으로 업그레이드된
-                  옵션을 선택하면 확인할 수 있습니다.{' '}
-                  <span style={{ color: '#ff4c4c' }}>
-                    (레전드리 업그레이드 경우 레전드리 옵션에서 확인할 수
-                    있습니다.)
-                  </span>
-                </Typography>
-                {!imageLoaded5 && (
-                  <Box
-                    sx={{
-                      width: '332.7px',
-                      height: '250px',
-                      display: 'flex',
-                      justifyContent: 'center',
-                      alignItems: 'center',
-                    }}
-                  >
-                    <CircularProgress />
-                  </Box>
-                )}
-                <img
-                  src="/images/upgrade_success.png"
-                  alt=""
-                  style={{
-                    maxWidth: '100%',
-                    height: '250px',
-                  }}
-                  onLoad={handleImageLoad5}
-                />
-              </Box>
-            </Box>
+            <GuideTextUI
+              title="사용법"
+              isImage={true}
+              description={[
+                {
+                  text: '데이터를 확인하고 싶은 캐릭터를 선택해 주세요.',
+                  coloredTexts: ['캐릭터'],
+                  imageSrc: '/images/select_character.gif',
+                  imageLoaded: imageLoaded1,
+                  handleImageLoad: handleImageLoad1,
+                  imageWidth: '332.7px',
+                  imageHeight: '130px',
+                },
+                {
+                  text: '캐릭터 선택 후 아이템을 선택해 주세요.',
+                  coloredTexts: ['아이템'],
+                  imageSrc: '/images/select_item.gif',
+                  imageLoaded: imageLoaded2,
+                  handleImageLoad: handleImageLoad2,
+                  imageWidth: '332.7px',
+                  imageHeight: '130px',
+                },
+                {
+                  text: '데이터를 확인하고 싶은 큐브를 선택해 주세요.                  ',
+                  coloredTexts: ['큐브'],
+                  imageSrc: '/images/select_cubename.gif',
+                  imageLoaded: imageLoaded3,
+                  handleImageLoad: handleImageLoad3,
+                  imageWidth: '332.7px',
+                  imageHeight: '75px',
+                },
+                {
+                  text: '데이터를 확인하고 싶은 큐브 옵션을 선택해 주세요.',
+                  coloredTexts: ['큐브 옵션'],
+                  imageSrc: '/images/selec_cubetype.gif',
+                  imageLoaded: imageLoaded4,
+                  handleImageLoad: handleImageLoad4,
+                  imageWidth: '332.7px',
+                  imageHeight: '75px',
+                },
+              ]}
+            />
+          </Box>
 
-            <Box sx={{ display: 'inline-flex' }}>
-              <Typography
-                variant="h6"
-                sx={{ display: 'inline-block', mr: 1, visibility: 'hidden' }}
-              >
-                ◈
-              </Typography>
-              <Box>
-                <Typography variant="body2" sx={{ mb: 1 }}>
-                  옵션 상세정보에는 큐브 옵션에 대한 등급이 표시되어 있습니다.{' '}
-                  <span style={{ color: '#ff4c4c' }}>
-                    (이탈 옵션 확인이 가능합니다.)
-                  </span>
-                </Typography>
-                {!imageLoaded6 && (
-                  <Box
-                    sx={{
-                      width: '332.7px',
-                      height: '115px',
-                      display: 'flex',
-                      justifyContent: 'center',
-                      alignItems: 'center',
-                    }}
-                  >
-                    <CircularProgress />
-                  </Box>
-                )}
-                <img
-                  src="/images/top_option.png"
-                  alt=""
-                  style={{
-                    maxWidth: '100%',
-                    height: '115px',
-                  }}
-                  onLoad={handleImageLoad6}
-                />
-              </Box>
-            </Box>
-
-            <Box sx={{ display: 'inline-flex' }}>
-              <Typography
-                variant="h6"
-                sx={{ display: 'inline-block', mr: 1, visibility: 'hidden' }}
-              >
-                ◈
-              </Typography>
-              <Box>
-                <Typography variant="body2" sx={{ mb: 1 }}>
-                  미라클 타임 및 큐브 천장에 대한 상세 정보도 확인할 수
-                  있습니다.
-                </Typography>
-                <Box sx={{ display: fullScreen ? 'inline-block' : 'flex' }}>
-                  {!imageLoaded7 && (
-                    <Box
-                      sx={{
-                        width: '332.7px',
-                        height: '115px',
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                      }}
-                    >
-                      <CircularProgress />
-                    </Box>
-                  )}
-                  {!imageLoaded8 && (
-                    <Box
-                      sx={{
-                        width: '332.7px',
-                        height: '115px',
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                      }}
-                    >
-                      <CircularProgress />
-                    </Box>
-                  )}
-                </Box>
-
-                <img
-                  src="/images/upgradeguarantee_success.png"
-                  alt=""
-                  style={{
-                    maxWidth: '100%',
-                    height: '115px',
-                  }}
-                  onLoad={handleImageLoad7}
-                />
-                <img
-                  src="/images/miracle_time_flag.png"
-                  alt=""
-                  style={{
-                    maxWidth: '100%',
-                    height: '115px',
-                  }}
-                  onLoad={handleImageLoad8}
-                />
-              </Box>
-            </Box>
+          <Box sx={{ mb: 2 }}>
+            <GuideTextUI
+              title="큐브 옵션 확인"
+              isImage={true}
+              description={[
+                {
+                  text: '큐브 등급 업을 확인하고 싶은 경우 해당 등급으로 업그레이드된 옵션을 선택하면 확인할 수 있습니다. (레전드리 업그레이드 경우 레전드리 옵션에서 확인할 수 있습니다.)',
+                  coloredTexts: [
+                    '(레전드리 업그레이드 경우 레전드리 옵션에서 확인할 수 있습니다.)',
+                  ],
+                  imageSrc: '/images/upgrade_success.png',
+                  imageLoaded: imageLoaded5,
+                  handleImageLoad: handleImageLoad5,
+                  imageWidth: '332.7px',
+                  imageHeight: '250px',
+                },
+                {
+                  text: '옵션 상세정보에는 큐브 옵션에 대한 등급이 표시되어 있습니다. (이탈 옵션 확인이 가능합니다.)',
+                  coloredTexts: ['(이탈 옵션 확인이 가능합니다.)'],
+                  imageSrc: '/images/top_option.png',
+                  imageLoaded: imageLoaded6,
+                  handleImageLoad: handleImageLoad6,
+                  imageWidth: '332.7px',
+                  imageHeight: '115px',
+                },
+                {
+                  text: '미라클 타임 및 큐브 천장에 대한 상세 정보도 확인할 수 있습니다.',
+                  coloredTexts: ['미라클 타임', '큐브 천장'],
+                  imageSrc: [
+                    {
+                      src: '/images/upgradeguarantee_success.png',
+                      imageLoaded: imageLoaded7,
+                      handleImageLoad: handleImageLoad7,
+                    },
+                    {
+                      src: '/images/miracle_time_flag.png',
+                      imageLoaded: imageLoaded8,
+                      handleImageLoad: handleImageLoad8,
+                    },
+                  ],
+                  imageWidth: '332.7px',
+                  imageHeight: '115px',
+                },
+              ]}
+            />
           </Box>
         </Container>
       </DialogContent>
